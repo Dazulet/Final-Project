ec323ddda5d36172d1536ca56a95570b025c0396
Assilbekov Didar <158120644+Didvr02@users.noreply.github.com>
2025-04-30 19:22:37 +0500

    Update Gargoyle.java

--- src/entity/Gargoyle.java
+++ src/entity/Gargoyle.java
@@ -2,4 +2,21 @@ package entity;
 
 
 
-public class Gargoyle extends Entity{}
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import src.main.GamePanel;
+
+public class Gargoyle extends Entity{
+    
+    GamePanel gp;
+    public static final int number = 5;
+    
+    public Gargoyle(GamePanel gp, int col, int row) {
+        this.gp = gp;
+        setDefaultValues(col, row);
+        getImage();
+    }
