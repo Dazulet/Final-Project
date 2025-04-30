b5b472913765d4312f491d30d5f288a22f6e85b5
abissaliyeva <aruzhan.abissaliyeva@narxoz.kz>
2025-05-01 04:09:06 +0600

    Update Goblin.java
    added main variables, constructor and set defaut values

--- src/entity/Goblin.java
+++ src/entity/Goblin.java
@@ -1,3 +1,35 @@
 package entity;
 
-public class Goblin extends Entity{}
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import src.main.GamePanel;
+
+public class Goblin extends Entity{
+    
+    GamePanel gp;
+    public static final int number = 10;
+    
+    public Goblin(GamePanel gp, int col, int row) {
+        this.gp = gp;
+        setDefaultValues(col, row);
+        getImage();
+    }
+    
+    public void setDefaultValues(int col, int row) {
+        this.x = col*gp.tileSize;
+        this.y = row*gp.tileSize;
+        hit_point = 100;
+        visable = false;
+        direction = UP;
+        symbol = GOBLIN;
+    }
+  
+  //public void getImage() 
+  
+  //public void draw(Graphics2D g2)
+}
+}
