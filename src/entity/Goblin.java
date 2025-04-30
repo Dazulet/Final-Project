43e0ccbd5a4d871306c39e8f5192f4be6af7c595
abissaliyeva <aruzhan.abissaliyeva@narxoz.kz>
2025-05-01 05:11:03 +0600

    added getImage, draw classes

--- src/entity/Goblin.java
+++ src/entity/Goblin.java
@@ -27,9 +27,63 @@ public class Goblin extends Entity{
         direction = UP;
         symbol = GOBLIN;
     }
+
+    public void getImage() {
+        try {
+            standingUp = new BufferedImage[2];
+            standingUp[0] = ImageIO.read(getClass().getResourceAsStream("/res/goblin/standing up 1.png"));
+            standingUp[1] = ImageIO.read(getClass().getResourceAsStream("/res/goblin/standing up 2.png"));
+            
+            standingDown = new BufferedImage[2];
+            standingDown[0] = ImageIO.read(getClass().getResourceAsStream("/res/goblin/standing down 1.png"));
+            standingDown[1] = ImageIO.read(getClass().getResourceAsStream("/res/goblin/standing down 2.png"));
+            
+            standingLeft = new BufferedImage[2];
+            standingLeft[0] = ImageIO.read(getClass().getResourceAsStream("/res/goblin/standing left 1.png"));
+            standingLeft[1] = ImageIO.read(getClass().getResourceAsStream("/res/goblin/standing left 2.png"));
+            
+            standingRight = new BufferedImage[2];
+            standingRight[0] = ImageIO.read(getClass().getResourceAsStream("/res/goblin/standing right 1.png"));
+            standingRight[1] = ImageIO.read(getClass().getResourceAsStream("/res/goblin/standing right 2.png"));
+            
+        }catch(IOException e) {
+            e.printStackTrace();            
+        }
+    }
   
-  //public void getImage() 
-  
-  //public void draw(Graphics2D g2)
-}
+  public void draw(Graphics2D g2) {
+        if(!visable)
+            return;
+        
+        spriteCounter++;
+        if(spriteCounter > 12) {
+            if(spriteNum == 1)
+                spriteNum = 0;
+            else spriteNum++;
+            spriteCounter = 0;
+        }
+        
+        BufferedImage image = null;
+        
+        
+        switch(direction) {
+            case UP:
+                image = standingUp[spriteNum%2];
+                break;
+            case DOWN:
+                image = standingDown[spriteNum%2]; 
+                break;
+            case LEFT:
+                image = standingLeft[spriteNum];
+                break;
+            case RIGHT:
+                image = standingRight[spriteNum];
+                break;
+        }
+        
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        
+//        g2.setColor(Color.green);
+//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+    }
 }
