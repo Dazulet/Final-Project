41293c04047a0123cefd31f245a59bf91dc79df9
abissaliyeva <aruzhan.abissaliyeva@narxoz.kz>
2025-05-01 16:14:28 +0600

    created main parts of Zork

--- src/entity/Zork.java
+++ src/entity/Zork.java
@@ -1,5 +1,32 @@
 package entity;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 
-public class Zork extends Entity{}
+import src.main.GamePanel;
+
+public class Zork extends Entity{
+    GamePanel gp;
+    static final int number = 1;
+    
+    public Zork(GamePanel gp, int col, int row) {
+        this.gp = gp;
+        setDefaultValues(col, row);
+        getImage();
+    }
+    
+    public void setDefaultValues(int col, int row) {
+        this.x = col*gp.tileSize;
+        this.y = row*gp.tileSize;
+        hit_point = 500;
+        visable = false;
+        direction = DOWN;
+        symbol = ZORK;
+    }
+    
+    
+
+}
