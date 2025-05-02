16aabd31f3397ad0fbf2ac4e50750e6ed296cf92
Sovetov Birzhan <bsovetov301@gmail.com>
2025-05-02 22:27:58 +0500

    Updated KeyHandler and CollisionChecker

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -1,3 +1,78 @@
 package main;
 
-public class CollisionChecker {}
+import entity.Entity;
+import tile.TileManager;
+
+public class CollisionChecker {
+
+    GamePanel gp;
+
+    public CollisionChecker(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void checkTile() {
+        gp.player.collisionOn = false;
+
+        int playerCol = gp.player.x/gp.tileSize;
+        int playerRow = gp.player.y/gp.tileSize;
+
+        int tileNum;
+
+        switch(gp.player.direction) {
+            case Entity.UP:
+                tileNum = gp.tileM.mapTileNum[playerCol][playerRow-1];
+                if(gp.tileM.tiles[tileNum].collision)
+                    gp.player.collisionOn = true;
+                break;
+            case Entity.DOWN:
+                tileNum = gp.tileM.mapTileNum[playerCol][playerRow+1];
+                if(gp.tileM.tiles[tileNum].collision)
+                    gp.player.collisionOn = true;
+                break;
+            case Entity.RIGHT:
+                tileNum = gp.tileM.mapTileNum[playerCol+1][playerRow];
+                if(gp.tileM.tiles[tileNum].collision)
+                    gp.player.collisionOn = true;
+                break;
+            case Entity.LEFT:
+                tileNum = gp.tileM.mapTileNum[playerCol-1][playerRow];
+                if(gp.tileM.tiles[tileNum].collision)
+                    gp.player.collisionOn = true;
+                break;
+
+        }
+    }
+
+    public void checkVillages() {
+        int playerCol = gp.player.x/gp.tileSize;
+        int playerRow = gp.player.y/gp.tileSize;
+        int tileNum = gp.tileM.mapTileNum[playerCol][playerRow];
+
+
+        switch(gp.tileM.tiles[tileNum].symbol) {
+            case TileManager.village_a:
+                gp.player.hit_point += 200;
+                gp.tileM.tiles[tileNum].symbol = ' ';
+                break;
+            case TileManager.village_b:
+                gp.player.hit_point += 150;
+                gp.tileM.tiles[tileNum].symbol = ' ';
+                break;
+            case TileManager.village_c:
+                gp.player.hit_point += 100;
+                gp.tileM.tiles[tileNum].symbol = ' ';
+                break;
+        }
+    }
+
+    public void checkMines() {
+        for(int i = 0; i < gp.minesM.mines.length; i++) {
+            if(gp.minesM.mines[i] != null && gp.player.x == gp.minesM.mines[i].x && gp.player.y == gp.minesM.mines[i].y) {
+                gp.minesM.setExplosion(gp.minesM.mines[i]);
+                gp.minesM.mines[i] = null;
+                gp.player.hit_point -= 100;
+            }
+        }
+    }
+}
--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -1,5 +1,133 @@
 package main;
-
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class KeyHandler implements KeyListener{}
+public class KeyHandler implements KeyListener{
+    public class KeyHandler implements KeyListener{
+
+        public boolean upPressed, downPressed, leftPressed, rightPressed, rPressed;
+        GamePanel gp;
+
+        public KeyHandler(GamePanel gp) {
+            this.gp = gp;
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int code = e.getKeyCode();
+
+            if(code == KeyEvent.VK_ESCAPE)
+                gp.gameThread = null;
+
+            if(code == KeyEvent.VK_M)
+                gp.toggle_sound();
+
+            if(gp.panelState == gp.START) {
+                if(code == KeyEvent.VK_W) {
+                    gp.ss.commandNum--;
+                    if(gp.ss.commandNum == -1)
+                        gp.ss.commandNum = 2;
+                }
+                else if(code == KeyEvent.VK_S) {
+                    gp.ss.commandNum++;
+                    if(gp.ss.commandNum == 3)
+                        gp.ss.commandNum = 0;
+                }
+                if(code == KeyEvent.VK_ENTER) {
+                    switch(gp.ss.commandNum) {
+                        case 0:
+                            gp.panelState = gp.GAME;
+                            break;
+                        case 1:
+                            gp.panelState = gp.LEVEL;
+                            break;
+                        case 2:
+                            gp.gameThread = null;
+                    }
+                }
+            }
+            else if(gp.panelState == gp.GAME) {
+                if(code == KeyEvent.VK_W) {
+                    upPressed = true;
+                }
+                else if(code == KeyEvent.VK_S) {
+                    downPressed = true;
+                }
+                else if(code == KeyEvent.VK_D) {
+                    rightPressed = true;
+                }
+                else if(code == KeyEvent.VK_A) {
+                    leftPressed = true;
+                }
+                else if(code == KeyEvent.VK_R) {
+                    rPressed = true;
+                }
+            }
+            else if(gp.panelState == gp.LEVEL) {
+                if(code == KeyEvent.VK_A && gp.ls.commandNum != 0) {
+                    gp.ls.commandNum--;
+                    gp.ls.level += 100;
+                }
+                if(code == KeyEvent.VK_D && gp.ls.commandNum != 2) {
+                    gp.ls.commandNum++;
+                    gp.ls.level -= 100;
+                }
+                if(code == KeyEvent.VK_ENTER) {
+                    gp.player.hit_point = gp.ls.level;
+                    gp.panelState = gp.START;
+                }
+            }
+            else if(gp.panelState == gp.END) {
+                if(code == KeyEvent.VK_W) {
+                    gp.es.commandNum--;
+                    if(gp.es.commandNum == -1)
+                        gp.es.commandNum = 1;
+                }
+                else if(code == KeyEvent.VK_S) {
+                    gp.es.commandNum++;
+                    if(gp.es.commandNum == 2)
+                        gp.es.commandNum = 0;
+                }
+                if(code == KeyEvent.VK_ENTER) {
+                    switch(gp.es.commandNum) {
+                        case 0:
+                            gp.panelState = gp.START;
+                            gp.restart();
+                            break;
+                        case 1:
+                            gp.gameThread = null;
+                            break;
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int code = e.getKeyCode();
+
+            if(code == KeyEvent.VK_W) {
+                upPressed = false;
+            }
+            else if(code == KeyEvent.VK_S) {
+                downPressed = false;
+            }
+            else if(code == KeyEvent.VK_D) {
+                rightPressed = false;
+            }
+            else if(code == KeyEvent.VK_A) {
+                leftPressed = false;
+            }
+            else if(code == KeyEvent.VK_R) {
+                rPressed = false;
+            }
+        }
+
+    }
+
+}
