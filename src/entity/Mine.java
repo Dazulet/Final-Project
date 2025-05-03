3b8d73ee39dc6d590982251f1b462d0a870aa3ec
abissaliyeva <aruzhan.abissaliyeva@narxoz.kz>
2025-05-03 17:47:14 +0600

    added constuctor

--- src/entity/Mine.java
+++ src/entity/Mine.java
@@ -1,3 +1,21 @@
 package entity;
 
-public class Mine extends Entity{}
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.Objects;
+import src.main.GamePanel;
+
+public class Mine extends Entity {
+    GamePanel gp;
+    public static final int number = 5;
+    final int damage = 100;
+
+    public Mine(GamePanel gp, int col, int row) {
+        this.gp = gp;
+        Objects.requireNonNull(gp);
+        this.x = col * 48;
+        Objects.requireNonNull(gp);
+        this.y = row * 48;
+        this.symbol = 'm';
+    }
+}
