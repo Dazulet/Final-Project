f933924183ab1b0077cbd1d30f4061febd29632b
Sovetov Birzhan <bsovetov301@gmail.com>
2025-05-08 15:45:32 +0500

    Updated Gargoyle added combat stats and improved animation logic

--- src/entity/Gargoyle.java
+++ src/entity/Gargoyle.java
@@ -1,46 +1,54 @@
-package entity;
-
-
+package src.entity;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
 import src.main.GamePanel;
 
 public class Gargoyle extends Entity{
-    
+
     GamePanel gp;
     public static final int number = 5;
-    
+
+    private int initialCol, initialRow;
+
     public Gargoyle(GamePanel gp, int col, int row) {
         this.gp = gp;
-        setDefaultValues(col, row);
+        this.initialCol = col;
+        this.initialRow = row;
+        setDefaultValues();
         getImage();
     }
 
+    @Override
+    public void setDefaultValues() {
+        super.setDefaultValues();
 
-public void setDefaultValues(int col, int row) {
-        this.x = col*gp.tileSize;
-        this.y = row*gp.tileSize;
-        hit_point = 150;
+        this.x = initialCol * gp.tileSize;
+        this.y = initialRow * gp.tileSize;
+
+        max_hit_point = 150;
+        hit_point = max_hit_point;
         visable = false;
         direction = LEFT;
         symbol = GARGOYLE;
+
+        experienceDropped = 50;
+        critChance = 0.06;
+        critDamageMultiplier = 1.5;
     }
 
- public void getImage() {
+    public void getImage() {
         try {
             standingUp = new BufferedImage[2];
             standingUp[0] = ImageIO.read(getClass().getResourceAsStream("/res/gargoyle/standing up 1.png"));
             standingUp[1] = ImageIO.read(getClass().getResourceAsStream("/res/gargoyle/standing up 2.png"));
-            
+
             standingDown = new BufferedImage[2];
             standingDown[0] = ImageIO.read(getClass().getResourceAsStream("/res/gargoyle/standing down 1.png"));
             standingDown[1] = ImageIO.read(getClass().getResourceAsStream("/res/gargoyle/standing down 2.png"));
-            
+
             standingLeft = new BufferedImage[2];
             standingLeft[0] = ImageIO.read(getClass().getResourceAsStream("/res/gargoyle/standing left 1.png"));
             standingLeft[1] = ImageIO.read(getClass().getResourceAsStream("/res/gargoyle/standing left 2.png"));
@@ -48,43 +56,37 @@ public void setDefaultValues(int col, int row) {
             standingRight = new BufferedImage[2];
             standingRight[0] = ImageIO.read(getClass().getResourceAsStream("/res/gargoyle/standing right 1.png"));
             standingRight[1] = ImageIO.read(getClass().getResourceAsStream("/res/gargoyle/standing right 2.png"));
-            
-            
+
         }catch(IOException e) {
-            e.printStackTrace();            
+            e.printStackTrace();
+        }catch(IllegalArgumentException e) {
+            System.err.println("Error loading gargoyle images: " + e.getMessage());
+            e.printStackTrace();
         }
     }
-    
+
+    @Override
     public void draw(Graphics2D g2) {
         if(!visable)
             return;
-        
+
         spriteCounter++;
         if(spriteCounter > 12) {
-            if(spriteNum == 1)
-                spriteNum = 0;
-            else spriteNum++;
+            spriteNum = (spriteNum + 1) % 2;
             spriteCounter = 0;
         }
-        
+
         BufferedImage image = null;
-        
-        
         switch(direction) {
-            case UP:
-                image = standingUp[spriteNum%2];
-                break;
-            case DOWN:
-                image = standingDown[spriteNum%2]; 
-                break;
-            case LEFT:
-                image = standingLeft[spriteNum];
-                break;
-            case RIGHT:
-                image = standingRight[spriteNum];
-                break;
+            case UP:    image = standingUp[spriteNum];    break;
+            case DOWN:  image = standingDown[spriteNum];  break;
+            case LEFT:  image = standingLeft[spriteNum];  break;
+            case RIGHT: image = standingRight[spriteNum]; break;
+            default:    image = standingDown[spriteNum];  break;
         }
-        
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+
+        if (image != null) {
+            g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        }
     }
 }
