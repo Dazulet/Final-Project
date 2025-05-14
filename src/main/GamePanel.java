b3cd5d83a12a5f0865d365ce972cbb0ebbc61aa9
Daulet <baltashdaulet3@gmail.com>
2025-05-14 16:55:34 +0500

    update Main , GamePanel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,166 +1,334 @@
-package main;
+package src.main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-
+import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import javax.swing.JPanel;
 
-import entity.Player;
-import screens.EndScreen;
-import screens.LevelScreen;
-import screens.StartScreen;
-import tile.TileManager;
+import src.entity.Player;
+import src.screens.EndScreen;
+import src.screens.LevelScreen;
+import src.screens.StartScreen;
+import src.tile.TileManager;
+import src.main.ItemManager;
+import src.main.MonstersManager;
+import src.main.MinesManager;
 
 public class GamePanel extends JPanel implements Runnable{
 
-    final int originalTileSize = 16;
-    final int scale = 3;
+	final int originalTileSize = 16;
+	final int scale = 3;
+	public final int tileSize = originalTileSize * scale;
 
-    public final int tileSize = originalTileSize * scale;
-    public final int maxScreenCol = 19;
-    public final int maxScreenRow = 16;
-    public final int screenWidth = tileSize * maxScreenCol;
-    public final int screenHeight = tileSize * maxScreenRow;
+	public final int maxScreenCol = 19;
+	public final int maxScreenRow = 16;
+	public final int screenWidth = tileSize * maxScreenCol;
+	public final int screenHeight = tileSize * maxScreenRow;
 
-    public int panelState;
-    public final int START = 0;
-    public final int GAME = 1;
-    public final int LEVEL = 2;
-    public final int END = 3;
+	int screenWidthActual;
+	int screenHeightActual;
 
-    final int FPS = 60;
+	public int panelState;
+	public final int START = 0;
+	public final int GAME = 1;
+	public final int LEVEL = 2;
+	public final int END = 3;
+	public final int FPS = 60;
+	private boolean sound_off = false;
 
-    private boolean sound_off = false;
+	public KeyHandler keyH = new KeyHandler(this);
+	public UI ui = new UI(this);
+	public StartScreen ss = new StartScreen(this);
+	public EndScreen es = new EndScreen(this);
+	public LevelScreen ls = new LevelScreen(this);
+	public TileManager tileM;
+	public CollisionChecker cChecker = new CollisionChecker(this);
+	public BattleManager battleM = new BattleManager(this);
+	public MonstersManager monstersM;
+	public MinesManager minesM;
+	public ItemManager itemM;
+	public Sound sound = new Sound();
+	Thread gameThread;
 
-    public KeyHandler keyH = new KeyHandler(this);
-    public UI ui = new UI(this);
-    public StartScreen ss = new StartScreen(this);
-    public EndScreen es = new EndScreen(this);
-    public LevelScreen ls = new LevelScreen(this);
-    public TileManager tileM = new TileManager(this);
-    public CollisionChecker cChecker = new CollisionChecker(this);
-    public BattleManager battleM = new BattleManager(this);
-    public MonstersManager monstersM = new MonstersManager(this);
-    public MinesManager minesM = new MinesManager(this);
-    public Player player = new Player(this);
-    public Sound sound = new Sound();
-    Thread gameThread;
+	public Player player = new Player(this);
+	public int currentLevel = 1;
+	public final int MAX_LEVELS = 3;
+	private String transitionMessage = null;
+	private int transitionMessageTimer = 0;
+	private List<String> mapFiles;
+	private Random randomMapSelector;
+	private String currentMapPath;
 
-    public GamePanel() {
-        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.black);
-        this.setDoubleBuffered(true);
-        this.addKeyListener(keyH);
-        this.setFocusable(true);
-    }
+	public GamePanel() {
 
-    public void startGameThread() {
-        gameThread = new Thread(this);
-        panelState = START;
-        gameThread.start();
-    }
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		screenWidthActual = screenSize.width;
+		screenHeightActual = screenSize.height;
 
-    @Override
-    public void run() {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 
-        double drawInterval = 1_000_000_000/FPS;
-        double delta = 0;
-        long lastTime = System.nanoTime();
-        long currentTime;
-        long timer = 0;
-        int drawCounter = 0;
+		mapFiles = new ArrayList<>();
+		mapFiles.add("/res/maps/map.txt");
+		mapFiles.add("/res/maps/map2.txt");
+		mapFiles.add("/res/maps/map3.txt");
 
-        while(gameThread != null) {
-            currentTime = System.nanoTime();
+		randomMapSelector = new Random();
 
-            delta += (currentTime - lastTime)/drawInterval;
-            timer += (currentTime - lastTime);
-            lastTime = currentTime;
+		if (!mapFiles.isEmpty()) {
+			setupNewGameComponents(mapFiles.get(0));
+		} else {
+			System.err.println("CRITICAL ERROR: Map files list is empty at GamePanel construction!");
 
-            if(delta >= 1) {
-                update();
-                repaint();
-                delta--;
-                drawCounter++;
-            }
+		}
+	}
 
-            if(timer >= 1_000_000_000) {
-                System.out.println("FPS " + drawCounter);
-                drawCounter = 0;
-                timer = 0;
-            }
+	private void setupNewGameComponents(String mapPath) {
+		this.currentMapPath = mapPath;
+		this.tileM = new TileManager(this, currentMapPath);
+		this.monstersM = new MonstersManager(this);
+		this.minesM = new MinesManager(this);
+		this.itemM = new ItemManager(this);
 
-        }
+		if (this.player != null) {
 
-        System.exit(0);
-    }
+		} else {
 
-    public void update() {
+			this.player = new Player(this);
+			System.err.println("Warning: Player was null during setupNewGameComponents. Recreated.");
+		}
+	}
 
-        if(panelState != GAME)
-            return;
+	public List<String> getMapFiles() {
+		return mapFiles;
+	}
 
-        player.update();
-        battleM.update();
+	public void startGameThread() {
+		gameThread = new Thread(this);
+		currentLevel = 1;
 
-    }
+		if (!mapFiles.isEmpty()) {
+			setupNewGameComponents(mapFiles.get(0));
+		} else {
+			System.err.println("CRITICAL ERROR: Map files list is empty! Cannot start game.");
+			return;
+		}
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+		if (player != null) {
+			player.setDefaultValues();
+		} else {
+			player = new Player(this);
+			player.setDefaultValues();
+		}
 
-        Graphics2D g2 = (Graphics2D)g;
+		panelState = START;
+		gameThread.start();
+	}
 
-        if(panelState == START) {
-            ss.draw(g2);
-        }
-        else if(panelState == GAME) {
-            tileM.draw(g2);
-            monstersM.draw(g2);
-            player.draw(g2);
-            ui.draw(g2);
-            minesM.explode(g2);
+	@Override
+	public void run() {
+		double drawInterval = 1_000_000_000.0 / FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		while (gameThread != null) {
+			currentTime = System.nanoTime();
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
+			if (delta >= 1) {
+				update();
+				repaint();
+				delta--;
+			}
+			if (timer >= 1_000_000_000) {
+				timer = 0;
+			}
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				Thread.currentThread().interrupt();
+			}
+		}
+		System.exit(0);
+	}
 
-        }
-        else if(panelState == LEVEL) {
-            ls.draw(g2);
-        }
-        else if(panelState == END) {
-            es.draw(g2);
-        }
+	public void update() {
+		if (transitionMessageTimer > 0) {
+			transitionMessageTimer--;
+			if (transitionMessageTimer == 0) {
+				transitionMessage = null;
+			}
+		}
+		if(panelState == GAME) {
+			if (player != null) player.update();
+			if (battleM != null) battleM.update();
+			if (itemM != null) itemM.update();
+			if (minesM != null) {  }
+			if (monstersM != null) {  }
+		}
+		else if (panelState == START) {
 
-        g2.dispose();
-    }
+		}
+		else if (panelState == LEVEL) {
 
-    public void playMusic(int i) {
+		}
+		else if (panelState == END) {
+		}
+	}
 
-        sound.setFile(i);
-        sound.play();
-        sound.loop();
-    }
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		int currentWidth = getWidth();
+		int currentHeight = getHeight();
+		if (currentWidth <= 0 || currentHeight <= 0) {
+			currentWidth = screenWidthActual;
+			currentHeight = screenHeightActual;
+		}
+		Graphics2D g2 = (Graphics2D)g.create();
+		if (currentWidth > 0 && currentHeight > 0 && screenWidth > 0 && screenHeight > 0) {
+			double scaleX = (double) currentWidth / screenWidth;
+			double scaleY = (double) currentHeight / screenHeight;
 
-    public void stopMusic() {
-        sound.stop();
-    }
+			g2.scale(scaleX, scaleY);
+		} else {
+			System.err.println("Warning: Invalid dimensions for scaling in paintComponent.");
+		}
+		if(panelState == START) {
+			if (ss != null) ss.draw(g2);
+		}
+		else if(panelState == GAME) {
 
-    public void playSE(int i) {
-        if(sound_off) return;
-        sound.setFile(i);
-        sound.play();
-    }
+			if (tileM != null) tileM.draw(g2);
+			if (itemM != null) itemM.draw(g2);
+			if (monstersM != null) monstersM.draw(g2);
+			if (player != null) player.draw(g2);
+			if (minesM != null) minesM.explode(g2);
+			if (ui != null) ui.draw(g2);
+		}
+		else if(panelState == LEVEL) {
+			if (ls != null) ls.draw(g2);
+		}
+		else if(panelState == END) {
+			if (es != null) es.draw(g2);
+		}
 
-    public void toggle_sound() {
-        sound_off = !sound_off;
+		if (transitionMessage != null && ui != null) {
+			ui.drawTransitionMessage(g2, transitionMessage);
+		}
 
-    }
+		g2.dispose();
+	}
 
-    void restart() {
-        monstersM = new MonstersManager(this);
-        minesM = new MinesManager(this);
-        player = new Player(this);
-        player.hit_point = ls.level;
-    }
+	public void playMusic(int i) { if(sound != null) {sound.setFile(i); sound.play(); sound.loop();} }
+	public void stopMusic() { if(sound != null) sound.stop(); }
+	public void playSE(int i) { if(sound_off || sound == null) return; sound.setFile(i); sound.play(); }
+	public void toggle_sound() { sound_off = !sound_off; }
 
+	public void prepareNextLevel() {
+		currentLevel++;
+		if (currentLevel > MAX_LEVELS) {
+
+			panelState = END;
+			if (es != null && player != null) es.setTitleTexts("CONGRATULATIONS!\nYou Beat All Levels!\nTotal Steps: " + player.steps);
+		} else {
+
+			showTransitionMessage("Level " + currentLevel, 120);
+
+			String nextMapPath = currentMapPath;
+			List<String> availableMaps = getMapFiles();
+			if (availableMaps != null && !availableMaps.isEmpty()) {
+				if (currentLevel -1 < availableMaps.size()) {
+					nextMapPath = availableMaps.get(currentLevel - 1);
+				} else {
+
+					nextMapPath = availableMaps.get(availableMaps.size() - 1);
+					System.out.println("Warning: currentLevel (" + currentLevel + ") exceeds map list size ("+ availableMaps.size() +"). Using last map: " + nextMapPath);
+				}
+			} else { System.err.println("Map files list is null or empty in prepareNextLevel!"); }
+
+			setupNewGameComponents(nextMapPath);
+
+			if (player != null) {
+				player.resetForNewLevel();
+			}
+
+			if (battleM != null && player != null) {
+				player.gameState = player.MOVE;
+				player.movingState = 0;
+				battleM.rolling = false;
+				battleM.diceCounter = 0;
+				battleM.battleState = 0;
+				battleM.monsterIndex = -1;
+				battleM.clearMessages();
+			}
+
+			panelState = GAME;
+		}
+	}
+
+	public void handleZorkDefeat(int defeatedZorkIndex) {
+		if (player == null || monstersM == null || battleM == null) return;
+
+		if (defeatedZorkIndex >= 0 && defeatedZorkIndex < monstersM.monsters.length && monstersM.monsters[defeatedZorkIndex] != null) {
+			player.gainExperience(monstersM.monsters[defeatedZorkIndex].experienceDropped);
+			monstersM.monsters[defeatedZorkIndex] = null;
+
+		}
+
+		player.gameState = player.MOVE;
+		player.movingState = 0;
+
+		battleM.rolling = false;
+		battleM.diceCounter = 0;
+		battleM.battleState = 0;
+		battleM.monsterIndex = -1;
+		battleM.clearMessages();
+
+		prepareNextLevel();
+	}
+
+	public void showTransitionMessage(String message, int durationFrames) {
+		this.transitionMessage = message;
+		this.transitionMessageTimer = durationFrames;
+
+	}
+
+	void restart() {
+		currentLevel = 1;
+		List<String> availableMaps = getMapFiles();
+		if (availableMaps != null && !availableMaps.isEmpty()) {
+
+			setupNewGameComponents(availableMaps.get(0));
+		} else {
+			System.err.println("CRITICAL ERROR: Map files list is null or empty! Cannot restart properly.");
+			return;
+		}
+
+		if (player == null) player = new Player(this);
+		player.setDefaultValues();
+
+		battleM = new BattleManager(this);
+		cChecker = new CollisionChecker(this);
+
+		player.gameState = player.MOVE;
+		player.movingState = 0;
+
+		showTransitionMessage("Level " + currentLevel, 120);
+		panelState = GAME;
+	}
+
+	public String getCurrentMapPath() { return currentMapPath; }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,71 @@
-package main;
+package src.main;
 
 import javax.swing.JFrame;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Main {
 
-    public static void main(String[] args) {
+	static GraphicsDevice device = GraphicsEnvironment
+			.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+	static JFrame frame;
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setTitle("Dungeon Survivor");
+	public static void main(String[] args) {
 
-        GamePanel gamePanel = new GamePanel();
-        frame.add(gamePanel);
-        frame.pack();
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setTitle("Dungeon Survivor");
 
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+		GamePanel gamePanel = new GamePanel();
+		frame.add(gamePanel);
 
-        gamePanel.startGameThread();
-    }
+		frame.setUndecorated(true);
 
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				exitFullScreen();
+				System.exit(0);
+			}
+		});
+
+		if (device.isFullScreenSupported()) {
+			try {
+				device.setFullScreenWindow(frame);
+				System.out.println("Fullscreen mode enabled.");
+			} catch (Exception e) {
+				System.err.println("Error setting fullscreen mode: " + e.getMessage());
+				device.setFullScreenWindow(null);
+				setupWindowedMode();
+			}
+		} else {
+			System.out.println("Fullscreen mode not supported by the system. Using windowed mode.");
+			setupWindowedMode();
+		}
+
+		frame.setVisible(true);
+
+		gamePanel.startGameThread();
+	}
+
+	private static void setupWindowedMode() {
+		frame.setUndecorated(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		for (WindowAdapter wl : frame.getListeners(WindowAdapter.class)) {
+			frame.removeWindowListener(wl);
+		}
+	}
+
+	public static void exitFullScreen() {
+		if (device.getFullScreenWindow() != null) {
+			device.setFullScreenWindow(null);
+			System.out.println("Exited fullscreen mode.");
+		}
+	}
 }
